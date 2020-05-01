19a5977917af6741bac48c2f4de27c922edc2b46
Jagat Singh <inboxjedi@gmail.com>
2020-05-01 07:21:49 +0000

    time complexity included

---
+++ selectionSort.js
@@ -0,0 +1,56 @@
+//suppose we have an array [21,34,1,4,34,87,3] 
+// we will have three pointer i, j, n(length of Array)
+// your i,n will define your processed array, array before i will be get sorted in each loop of i
+// [21,34,1,4,34,87,3] ,   i->21, j->34, n=>3, minimum number will be through linear comparison will be 1 at position 2, Swap your i -> 21 to min->1
+// [1,34,21,4,34,87,3] ,   i->34, j->21, n=>3, minimum number will be through linear comparison will be 3 at position 6, Swap your i -> 34 to min->3
+// [1,3,21,4,34,87,34] ,   i->21, j->4, n=>3, minimum number will be through linear comparison will be 4 at position 3, Swap your i -> 21 to min->4
+// [1,3,4,21,34,87,34]  repeat the same for each loop
+
+//Time complexity of Selection Sort is O(n2), as we loop over 2 times,
+//Space complexity of Selection Sort is O(n), 
+//as we dont use Extra space for any operation except Swap which max take n Swap. 
+
+
+function swapArrayValue(arr, index1, index2){
+    const temp = arr[index1];
+    arr[index1]= arr[index2];
+    arr[index2] = temp;
+    return arr;
+     
+}
+
+function selectionSort(arr){
+console.log("================Selection Sort============");
+console.log("Input :- ", arr);
+
+const arrLength =  arr.length;
+
+for(let i=0; i < arrLength-1; i++){
+  let min_index = i;
+  let j = i+1;
+   while(j<arrLength){
+       if(arr[j]<arr[min_index]){
+      min_index = j;
+      }
+        j++;
+   }
+  arr = swapArrayValue(arr,i,min_index);
+}
+console.log('output :- ',arr)
+  return arr;
+}
+
+selectionSort([21,34,1,4,34,87,3]);
+
+//======================Sort an array of String using Selection Sort===================
+//Question :- Sort an Array of String by Selection sort
+selectionSort
+([
+  'hello',
+  'mery',
+  'christmas',
+  'to',
+  'all',
+  'of',
+  'you'
+  ]);
